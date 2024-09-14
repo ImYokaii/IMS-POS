9debc1001c8f153cdde5ecd18898e63f9e3c8527
Masayuki Retuerma <90512033+ImYokaii@users.noreply.github.com>
2024-09-14 15:44:54 +0800

    Added Barcode Field

--- inventory_system/inventory_view/models.py
+++ inventory_system/inventory_view/models.py
@@ -6,6 +6,7 @@ class Product(models.Model):
     name = models.CharField(max_length=100, null=True)
     description = models.TextField(blank=True, null=True)
     sku = models.CharField(max_length=50, unique=True, null=True)
+    barcode = models.ImageField(upload_to='images/', default='barcodes/placeholder.jpg')
     category = models.CharField(max_length=50, null=True)
     price = models.DecimalField(max_digits=10, decimal_places=2, null=True)  # Selling price
     cost_price = models.DecimalField(max_digits=10, decimal_places=2, null=True)  # Purchase price
@@ -57,3 +58,4 @@ class WasteProduct(models.Model):
 
     def __str__(self):
         return f"Name: {self.name} (Date of Waste: {self.date_added})"
+    
