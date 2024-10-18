efcabe9928e277b4baa5d22393c8621357f21560
dadulsawol <apriljoydeang23@gmail.com>
2024-10-18 22:44:50 +0800

    Created new model and form named RequestQuotationItem
    a model and form for adding product entries to request quotation was made, and minor changes to request quotation fields are done.

--- inventory_system/procurement_view/forms.py
+++ inventory_system/procurement_view/forms.py
@@ -1,11 +1,24 @@
 from django import forms
-from .models import RequestQuotation
+from django.forms import modelformset_factory
+from .models import RequestQuotation, RequestQuotationItem
 
 class RequestQuotationForm(forms.ModelForm):
     class Meta:
+        STATUS_CHOICES = [('Ongoing', 'Ongoing'), ('Ended', 'Ended')]
+        
         model = RequestQuotation
+        
         fields = ['employee', 'buyer_company_name', 'buyer_address', 'buyer_contact', 'quotation_no', 'prepared_by', 'quote_valid_until',
-                'terms_and_conditions', 'total_amount', 'status',]
+                'terms_and_conditions', 'status',]
+        
         widgets = {
             'quote_valid_until': forms.DateInput(attrs={'type': 'date'}),
+            'status': forms.Select(choices=STATUS_CHOICES),
         }
+
+class RequestQuotationItemForm(forms.ModelForm):
+    class Meta:
+        model = RequestQuotationItem
+        fields = ['product_name', 'quantity', 'unit_price']
+
+RequestQuotationItemFormSet = modelformset_factory(RequestQuotationItem, form=RequestQuotationItemForm, extra=5)
--- inventory_system/procurement_view/models.py
+++ inventory_system/procurement_view/models.py
@@ -7,13 +7,21 @@ class RequestQuotation(models.Model):
     buyer_company_name = models.CharField(max_length=255)
     buyer_address = models.CharField(max_length=255)
     buyer_contact = models.CharField(max_length=100)
-    quotation_no = models.CharField(max_length=50, unique=True)
+    quotation_no = models.CharField(max_length=50, unique=True) # Revise to automatically create quotation no. upon saving
     prepared_by = models.CharField(max_length=100)
     quote_valid_until = models.DateField()
     date_prepared = models.DateField(auto_now_add=True)
     terms_and_conditions = models.TextField()
-    total_amount = models.DecimalField(max_digits=10, decimal_places=2)
     status = models.CharField(max_length=50)
 
     def __str__(self):
         return f"Quotation {self.quotation_no} for {self.buyer_company_name}"
+
+class RequestQuotationItem(models.Model):
+    request_quotation = models.ForeignKey(RequestQuotation, on_delete=models.CASCADE)
+    product_name = models.CharField(max_length=255)
+    quantity = models.IntegerField()
+    unit_price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.product_name} - {self.quantity} units"
