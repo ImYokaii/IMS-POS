0cc70c004df99a22091a2350812e4ebf46b32abd
SandorTheMoon <144748867+SandorTheMoon@users.noreply.github.com>
2024-10-31 14:13:55 +0800

    Temporarily deactivated automatic SKU Generator

--- inventory_system/inventory_view/models.py
+++ inventory_system/inventory_view/models.py
@@ -1,11 +1,8 @@
 import os
 from django.db import models
 from dotenv import load_dotenv
-import barcode
-from barcode.writer import ImageWriter
-from io import BytesIO
 from django.core.files import File
-from .utils import generate_digits
+# from .utils import generate_digits
 
 load_dotenv()
 
@@ -20,7 +17,7 @@ class Product(models.Model):
     PRODUCT_STATUS_CHOICES = product_status_choices
 
     name = models.CharField(max_length=100, null=True)
-    sku = models.CharField(max_length=50, unique=True, null=True)
+    sku = models.CharField(max_length=50, unique=True, null=True, blank=True)
     barcode = models.ImageField(upload_to='images/', default='barcodes/placeholder.jpg')
     category = models.CharField(max_length=50, null=True)
     selling_price = models.DecimalField(max_digits=10, decimal_places=2, null=True)  # Selling price
@@ -33,35 +30,22 @@ class Product(models.Model):
 
     def __str__(self):
         return f"Name: {self.name} (₱ {self.selling_price}) - {self.category}"
-    
-    def generate_batch_number(self):
-        if self.expiration_date:
-            return self.expiration_date.strftime('%m%d%y')
         
-        else:
-            return None
+    # def generate_sku_num(self):
+    #     category = self.category
         
-    def generate_sku_num(self):
-        category = self.category
+    #     if self.expiration_date:
+    #         type = True
         
-        if self.expiration_date:
-            type = True
-        
-        else:
-            type = False
+    #     else:
+    #         type = False
 
-        self.sku = generate_digits(category, type)
+    #     self.sku = generate_digits(category, type)
         
         
     def save(self):
-        if not self.sku:
-            self.generate_sku_num()
-        
-        EAN = barcode.get_barcode_class('ean13')
-        ean = EAN(f'{self.sku}',writer=ImageWriter())
-        buffer = BytesIO()
-        ean.write(buffer)
-        self.barcode.save(f'{self.name}_{'barcode.png'}', File(buffer), save=False )
+        # if not self.sku:
+        #     self.generate_sku_num()
 
         super(Product, self).save()
     
