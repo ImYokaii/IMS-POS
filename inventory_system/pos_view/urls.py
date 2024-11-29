049f23aefd1d12f78bce42707a1fdabad0857914
AlfredoDacquelJr <dacquelcaasialfredo@gmail.com>
2024-11-30 00:38:38 +0800

    POS Cash Processing

--- inventory_system/pos_view/urls.py
+++ inventory_system/pos_view/urls.py
@@ -4,7 +4,11 @@ from . import views
 urlpatterns = [
     path('pos_page/', views.pos_page, name='pos_page'),
     path('pos_page/add_item/', views.add_item, name='add_item'),
-    path('pos_page/complete_invoice/', views.complete_invoice, name='complete_invoice'),
     path('pos_page/edit_item/<int:item_id>/', views.edit_item, name='edit_item'),
     path('pos_page/delete_item/<int:item_id>/', views.delete_item, name='delete_item'),
+    path('pos_page/complete_invoice/', views.complete_invoice, name='complete_invoice'),
+    path('pos_page/input_cash/<int:invoice_id>/', views.input_cash, name='input_cash'),
+    path('pos_page/transaction_summary/<int:invoice_id>/', views.transaction_summary, name='transaction_summary'),
+    path('pos_page/finish_transaction/<int:invoice_id>/', views.finish_transaction, name='finish_transaction'),
 ]
+
