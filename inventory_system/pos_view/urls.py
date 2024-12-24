8108a32ee381e189969061d324f3e98acc1b058c
AlfredoDacquelJr <dacquelcaasialfredo@gmail.com>
2024-12-24 15:58:27 +0800

    added view details page for invoices and printing functionalities.

--- inventory_system/pos_view/urls.py
+++ inventory_system/pos_view/urls.py
@@ -3,13 +3,20 @@ from . import views
 
 urlpatterns = [
     path('pos_page/', views.pos_page, name='pos_page'),
-    path('pos_page/add_item/', views.add_item, name='add_item'),
-    path('pos_page/edit_item/<int:item_id>/', views.edit_item, name='edit_item'),
-    path('pos_page/delete_item/<int:item_id>/', views.delete_item, name='delete_item'),
-    path('pos_page/complete_invoice/', views.complete_invoice, name='complete_invoice'),
-    path('pos_page/input_cash/<int:invoice_id>/', views.input_cash, name='input_cash'),
-    path('pos_page/transaction_summary/<int:invoice_id>/', views.transaction_summary, name='transaction_summary'),
-    path('pos_page/finish_transaction/<int:invoice_id>/', views.finish_transaction, name='finish_transaction'),
-     path('pos_page/transaction_invoices/', views.transaction_invoices, name='transaction_invoices'),
+    
+    path('add_item/', views.add_item, name='add_item'),
+    path('edit_item/<int:item_id>/', views.edit_item, name='edit_item'),
+    path('delete_item/<int:item_id>/', views.delete_item, name='delete_item'),
+
+    path('complete_invoice/', views.complete_invoice, name='complete_invoice'),
+    path('input_cash/<int:invoice_id>/', views.input_cash, name='input_cash'),
+    path('transaction_summary/<int:invoice_id>/', views.transaction_summary, name='transaction_summary'),
+    path('finish_transaction/<int:invoice_id>/', views.finish_transaction, name='finish_transaction'),
+
+    path('transaction_invoices/', views.transaction_invoices, name='transaction_invoices'),
+    path('transaction_invoices_detail/<int:invoice_id>/', views.transaction_invoices_detail, name='transaction_invoices_detail'),
+
+    path('download_sales_invoice_pdf/<int:invoice_id>/', views.download_sales_invoice_pdf, name='download_sales_invoice_pdf'),
+    path('download_official_receipt_pdf/<str:invoice_no>/', views.download_official_receipt_pdf, name='download_official_receipt_pdf'),
 ]
 
