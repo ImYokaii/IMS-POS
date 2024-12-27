4205e653c9f6eedff872a21e58930c46e4e49ee6
Masayuki Retuerma <retuermamasayuki@gmail.com>
2024-12-27 21:49:30 +0800

    Modified Blank Graph
    - Added Simple Regression

--- inventory_system/dashboard_view/forms.py
+++ inventory_system/dashboard_view/forms.py
@@ -1,1 +1,16 @@
 from django import forms
+
+class ForecastForm(forms.Form):
+    FORECAST_CHOICES = [
+        ('daily', 'Daily'),
+        ('weekly', 'Weekly'),
+        ('monthly', 'Monthly'),
+        ('yearly', 'Yearly'),
+    ]
+
+    forecast_period = forms.ChoiceField(
+        choices=FORECAST_CHOICES,
+        label="Forecast Period",
+        widget=forms.Select(attrs={'class': 'form-control'}),
+        initial='monthly',
+    )
