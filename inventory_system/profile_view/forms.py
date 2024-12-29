fab52716ab8910acc5cd14a48fc252b0a6516070
AlfredoDacquelJr <dacquelcaasialfredo@gmail.com>
2024-12-29 22:43:00 +0800

    modified dashboard page to include both inventory and procurement overviews

--- inventory_system/profile_view/forms.py
+++ inventory_system/profile_view/forms.py
@@ -1,6 +1,8 @@
 from django import forms
 from django.contrib.auth.models import User
 from login_view.models import CompanyProfile
+from django.core.exceptions import ValidationError
+
 
 class UserProfileForm(forms.ModelForm):
     username = forms.CharField(required=True)
@@ -10,6 +12,23 @@ class UserProfileForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ['username', 'first_name', 'last_name']
+
+
+class PasswordChangeForm(forms.Form):
+    current_password = forms.CharField(widget=forms.PasswordInput, required=True, label="Current Password", min_length=8)
+    new_password = forms.CharField(widget=forms.PasswordInput, required=True, label="New Password", min_length=8)
+    confirm_password = forms.CharField(widget=forms.PasswordInput, required=True, label="Confirm New Password", min_length=8)
+
+    def clean(self):
+        cleaned_data = super().clean()
+        current_password = cleaned_data.get('current_password')
+        new_password = cleaned_data.get('new_password')
+        confirm_password = cleaned_data.get('confirm_password')
+
+        if new_password != confirm_password:
+            raise ValidationError('The new password and confirmation do not match.')
+
+        return cleaned_data
 
 
 class CompanyProfileForm(forms.ModelForm):
--- inventory_system/profile_view/urls.py
+++ inventory_system/profile_view/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('edit_profile/', views.edit_profile, name="edit_profile"),
+    path('change_password/', views.change_password, name='change_password'),
 ]
